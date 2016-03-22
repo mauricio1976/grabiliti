603a98e6a94777ab674d34a6db0835f8129fb649
Mauricio Chavez <mauricio.chavezr@gmail.com>
2016-03-22 00:10:27 -0500

    actulizacion boton en css border radius

--- js/seccion.js
+++ js/seccion.js
@@ -1,8 +1,37 @@
+//animate section
+$(document).ready(function() {
+    function close_accordion_section() {
+        $('.menu-btn').removeClass('active');
+        $('.mainnews').slideUp(300).removeClass('open');
+    }
+ 
+    $('.menu-btn').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+ 
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+ 
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.menu-btn' + currentAttrValue).slideDown(300).addClass('open'); 
+        }
+ 
+        e.preventDefault();
+    });
+});
+
+
+
+//transitions
 $(document).ready(function() {
      $('#ball1').click(function() {
           $('#mainnews1').animate({
                height: 'toggle'
-               }, 2000
+               }, 400
           );
      });
 });
@@ -11,7 +40,7 @@ $(document).ready(function() {
      $('#ball2').click(function() {
           $('#mainnews2').animate({
                height: 'toggle'
-               }, 2000
+               }, 400
           );
      });
 });
@@ -20,7 +49,7 @@ $(document).ready(function() {
      $('#ball3').click(function() {
           $('#mainnews3').animate({
                height: 'toggle'
-               }, 2000
+               }, 400
           );
      });
 });
@@ -29,10 +58,8 @@ $(document).ready(function() {
      $('#ball4').click(function() {
           $('#mainnews4').animate({
                height: 'toggle'
-               }, 2000
+               }, 400
           );
      });
 });
-		
-		
 		
